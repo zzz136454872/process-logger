3ff7c99c2bd411a967b97e6c3f5ab299ae163c65
f4prime <zzz136454872@163.com>
2020-02-16 17:42:30 +0800

    add config

--- log.py
+++ log.py
@@ -1,25 +1,36 @@
 from psutil import *
 from time import sleep
 
-#获取CPU不同状态运行时间
-print(cpu_times())
-print('CPU 执行用户进程时间：', cpu_times().user)
-print('CPU 执行系统调用时间：', cpu_times().system)
-print('CPU 空闲等待    时间：', cpu_times().idle)
-print('CPU 响应中断   时间：', cpu_times().interrupt)
-#CPU使用率：不加参数为上一次调用到现在使用率
-print('CPU      使用率：',cpu_percent())
-#3秒内CPU使用率
-print('CPU  3秒内使用率：',cpu_percent(interval=3))
-#3秒内每个CPU使用率
-print('每个逻辑CPU使用率：',cpu_percent(percpu = True))
-#CPU各个状态使用情况（百分比）
-print('CPU 各个状态使用情况：',cpu_times_percent())
-#每个CPU各个状态使用情况
-print('各个CPU 各个状态使用情况:')
-cpuinfos = cpu_times_percent(percpu = True)
-for info in cpuinfos:
-    print(info)
+#config 
+cpuinfo=True
+refresh = 5
+toFile=False
+filename='log.txt'
+hold=10
+
+
+if(cpu_info):
+    print("CPU核心数量: ",psutil.cpu_count(logical=False) # CPU物理核心
+    print("CPU线程数量: ",psutil.cpu_count() # CPU逻辑数量
+    #获取CPU不同状态运行时间
+    print(cpu_times())
+    print('CPU 执行用户进程时间：', cpu_times().user)
+    print('CPU 执行系统调用时间：', cpu_times().system)
+    print('CPU 空闲等待    时间：', cpu_times().idle)
+    print('CPU 响应中断   时间：', cpu_times().interrupt)
+    #CPU使用率：不加参数为上一次调用到现在使用率
+    print('CPU      使用率：',cpu_percent())
+    #3秒内CPU使用率
+    print('CPU  3秒内使用率：',cpu_percent(interval=3))
+    #3秒内每个CPU使用率
+    print('每个逻辑CPU使用率：',cpu_percent(percpu = True))
+    #CPU各个状态使用情况（百分比）
+    print('CPU 各个状态使用情况：',cpu_times_percent())
+    #每个CPU各个状态使用情况
+    print('各个CPU 各个状态使用情况:')
+    cpuinfos = cpu_times_percent(percpu = True)
+    for info in cpuinfos:
+        print(info)
 
 ps=[0 for i in range(65536)]
 i=0
@@ -44,7 +55,7 @@ while(True):
                 p=Process(pid)
                 ps[pid]=p
             tmp_time=ps[pid].cpu_percent()
-            if(tmp_time > 10):
+            if(tmp_time > hold):
                 print('pid: ',pid)
                 print('进程名称:', ps[pid].name())          #进程名称
                 #print('运行状态:', p.status())        #当前状态
@@ -59,7 +70,7 @@ while(True):
         except Exception:
             print("now")
             continue
-    sleep(5)
+    sleep(refresh)
 
 
 
