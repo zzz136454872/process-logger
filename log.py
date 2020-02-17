fc107095aba1be2a2a92d507ead82280b327d302
f4prime <zzz136454872@163.com>
2020-02-17 11:31:48 +0800

    use time del round

--- log.py
+++ log.py
@@ -1,29 +1,29 @@
 from psutil import *
 from time import sleep
-from logging import getLogger, FileHandler, info, StreamHandler,INFO,DEBUG
+from logging import getLogger, FileHandler, info, StreamHandler,INFO,DEBUG,Formatter
 
 #config 
 cpuinfo=True
 refresh = 5
-toFile=False
-filename='log.txt'
+toFile=True
+filename='cpu_use.log'
 hold=10
 
 logger=getLogger()
 logger.setLevel(DEBUG)
 console=StreamHandler()
 console.setLevel(INFO)
+formatter = Formatter("%(asctime)s - %(message)s")
+console.setFormatter(formatter)
 logger.addHandler(console)
-
 
 if toFile:
     filehandler=FileHandler(filename, mode='w')
     filehandler.setLevel(INFO)
+    filehandler.setFormatter(formatter)
     logger.addHandler(filehandler)
 
-
 if cpuinfo:
-    
     info("CPU核心数量: "+str(cpu_count(logical=False))) # CPU物理核心
     info("CPU线程数量: "+str(cpu_count())) # CPU逻辑数量
     #获取CPU不同状态运行时间
@@ -57,7 +57,7 @@ for pid in pid_list:
     ps[pid].cpu_percent()
 
 while(True):
-    info("round: %d",i)
+    #info("round: %d",i)
     i+=1
     pid_list = pids()
     for pid in pid_list:
@@ -69,17 +69,12 @@ while(True):
                 ps[pid]=p
             tmp_time=ps[pid].cpu_percent()
             if(tmp_time > hold):
-                info('pid: ',pid)
-                info('进程名称: %s', ps[pid].name())          #进程名称
-                #info('运行状态:', p.status())        #当前状态
-                #info('创建时间:', p.create_time())   #创建时间
-                info('CPU信息: %d',  ps[pid].cpu_times())     #进程的cpu时间信息,主要：user,system运行时间
-                info('CPU时间: %d', tmp_time)
-                #info('内存信息:', p.memory_percent())#进程内存利用率
-                #info('内存使用:', p.memory_info())   #进程内存使用详情
-                #info('IO信息：', p.io_counters() )   #进程的IO信息,包括读写IO数字及参数
-                #info('线程数：', p.num_threads() )   #进程开启的线程数
-                info()
+                info('pid: '+str(pid))
+                info('进程名称: '+str( ps[pid].name()))          #进程名称
+                #info('运行状态:'+str( p.status())        #当前状态
+                #info('创建时间:'+str( p.create_time())   #创建时间
+                info('CPU信息: '+str(  ps[pid].cpu_times()))     #进程的cpu时间信息+str(主要：user+str(system运行时间
+                info('CPU时间: '+str( tmp_time))
         except Exception:
             info("now")
             continue
